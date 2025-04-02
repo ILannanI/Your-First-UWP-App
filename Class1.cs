3063f2bedaba7d2d02ec4ff9d895fb8e9f22aa64
ILannanI <Sl0989031@otc.edu>
2025-04-01 23:30:20 -0500

    Your First UWP App

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StudentGradeManager
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+        public string Grade { get; set; }
+
+        public Student(string studentID, string firstName, string lastName, string className, string grade)
+        {
+            StudentID = studentID;
+            FirstName = firstName;
+            LastName = lastName;
+            ClassName = className;
+            Grade = grade;
+        }
+
+        public override string ToString()
+        {
+            return $"{StudentID} - {LastName}, {FirstName} - {ClassName} {Grade}";
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace StudentGradeManager
+{
+    public sealed partial class MainPage : Page
+    {
+        private List<Student> students;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            students = new List<Student>();
+        }
+
+        private void AddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string studentID = StudentID.Text;
+            string firstName = FirstName.Text;
+            string lastName = LastName.Text;
+            string className = ClassName.Text;
+            string grade = Grade.Text;
+
+            Student newStudent = new Student(studentID, firstName, lastName, className, grade);
+            students.Add(newStudent);
+
+            StudentID.Text = FirstName.Text = LastName.Text = ClassName.Text = Grade.Text = "";
+
+            StudentListBox.Items.Add(newStudent.ToString());
+        }
+
+        private async void SaveGrades_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            save
